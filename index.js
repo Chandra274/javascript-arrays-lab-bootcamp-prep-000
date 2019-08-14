e648e120321b4d65719518e26f71c91109e66171
Chandra Gopinathan <chandrag@globalcreditresearch.com>
2019-08-14 15:55:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+
 function destructivelyAppendKitten()
 {
   kittens.push("Ralph");
@@ -12,12 +13,20 @@ function destructivelyPrependKitten()
   kittens.unshift("Ralph");
   return kittens;
 }
+
 function destructivelyRemoveLastKitten()
 {
   kittens.pop();
   return kittens;
 }
+
 function destructivelyRemoveFirstKitten()
+{
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten()
 {
   kittens.shift();
   return kittens;
